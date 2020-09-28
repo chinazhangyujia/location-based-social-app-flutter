ce6c504a84f5c496cd0a0da281c9510c3f3890d2
chinazhangyujia <chinazhangyujia@gmail.com>
2020-09-27 23:59:01 -0400

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:location_based_social_app/screen/auth_screen.dart';
 import 'package:location_based_social_app/screen/new_post_screen.dart';
-import 'package:location_based_social_app/screen/post_home_screen.dart';
 import 'package:location_based_social_app/screen/tab_screen.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         accentColor: Colors.amber,
       ),
-      home: TabScreen(),
+      home: AuthScreen(),
       routes: {
         NewPostScreen.router: (context) => NewPostScreen()
       },
