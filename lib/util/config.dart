8534cca4109a31a7ee775d98c3ca0ac0ea7e5bbe
chinazhangyujia <chinazhangyujia@gmail.com>
2021-07-30 22:15:01 -0400

    update supported version

--- lib/util/config.dart
+++ lib/util/config.dart
@@ -1,5 +1,5 @@
 const String _PROD_SERVICE_DOMAIN = 'https://location-based-social-app-0.herokuapp.com'; // deployed service on heroku
 const String _DEV_SERVICE_DOMAIN = 'http://localhost:3000'; // local test service
 
-final String SERVICE_DOMAIN = _DEV_SERVICE_DOMAIN;
+final String SERVICE_DOMAIN = _PROD_SERVICE_DOMAIN;
 final String WEBSOCKET_DOMAIN = SERVICE_DOMAIN.replaceAll('http', 'ws');
