e3d94ed61fe08540eb0c2e2917ac829db818dfab
chinazhangyujia <chinazhangyujia@gmail.com>
2021-08-03 00:23:13 -0400

    add pick from camera for production

--- lib/util/config.dart
+++ lib/util/config.dart
@@ -5,7 +5,8 @@ const String _PROD_SERVICE_DOMAIN =
 const String _DEV_SERVICE_DOMAIN =
     'http://localhost:3000'; // local test service
 
-final String SERVICE_DOMAIN = mode == Mode.DEV ? _DEV_SERVICE_DOMAIN : _PROD_SERVICE_DOMAIN;
+final String SERVICE_DOMAIN =
+    mode == Mode.DEV ? _DEV_SERVICE_DOMAIN : _PROD_SERVICE_DOMAIN;
 final String WEBSOCKET_DOMAIN = SERVICE_DOMAIN.replaceAll('http', 'ws');
 
 enum Mode { DEV, PRODUCTION }
--- lib/widget/create_post/gallery_image_picker.dart
+++ lib/widget/create_post/gallery_image_picker.dart
@@ -45,35 +45,35 @@ class GalleryImagePicker extends StatelessWidget {
         mainAxisSpacing: 5,
       ),
       children: [
-        ...pickedImages.map((image) => Container(
-          decoration: BoxDecoration(
-              border: Border.all(width: 1, color: Colors.grey),
-              borderRadius: BorderRadius.circular(10)
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.file(
-              image,
-              fit: BoxFit.cover,
-              width: double.infinity,
+        ...pickedImages
+            .map((image) => Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.file(
+                      image,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  ),
+                ))
+            .toList(),
+        if (pickedImages.length < 6)
+          GestureDetector(
+            onTap: () {
+              pickImage(context);
+            },
+            behavior: HitTestBehavior.opaque,
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Icon(Icons.add),
             ),
-          ),
-        )).toList(),
-        if (pickedImages.length < 6) GestureDetector(
-          onTap: () {
-            pickImage(context);
-          },
-          behavior: HitTestBehavior.opaque,
-          child: Container(
-            decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.grey),
-                borderRadius: BorderRadius.circular(10)
-            ),
-            child: const Icon(Icons.add),
-          ),
-        )
+          )
       ],
     );
   }
 }
-
